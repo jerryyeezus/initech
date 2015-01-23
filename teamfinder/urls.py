7fdf866ac0b1828a21d4c8b95c3d2a515b98e716
yee <yee@localhost.localdomain>
2015-01-23 02:27:23 -0500

    Update detail urls.py

--- teamfinder/urls.py
+++ teamfinder/urls.py
@@ -4,5 +4,6 @@ from teamfinder import views
 
 urlpatterns = [
     url(r'^students/$', views.StudentList.as_view()),
-    url(r'^students/(?P<pk>[0-9]+)/$', views.StudentDetail.as_view())
+    # url(r'^students/(?P<pk>[0-9]+)/$', views.StudentDetail.as_view())
+    url(r'^students/(?P<pk>\w+)/$', views.StudentDetail.as_view())
 ]
