110e2623d6b7c218dfeedab46b62cdee6d4a7df9
Ubuntu <ubuntu@ip-172-31-4-87.us-west-2.compute.internal>
2015-02-05 23:01:02 +0000

    Remove CRM since useless

--- teamfinder/models.py
+++ teamfinder/models.py
@@ -57,9 +57,6 @@ class Course(models.Model):
     # Course ID
     course_id = models.IntegerField(blank=False)
 
-    # Course CRM
-    course_crm = str(course_dept) + ' ' + str(course_id)
-
     # Course name
     course_name = models.CharField(blank=False, max_length=64)
 
